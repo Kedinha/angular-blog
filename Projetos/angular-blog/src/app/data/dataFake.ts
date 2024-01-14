7639c6c7a9e2726f73701490e3e3b21703d1e476
kedinha <kedmyknow@gmail.com>
2024-01-14 20:32:08 -0300

    Content Pages Dynamic

--- Projetos/angular-blog/src/app/data/dataFake.ts
+++ Projetos/angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,40 @@
 export const dataFake = [
   {
-    id: 1,
-    photo: 'news',
-    title: 'News AI',
-    description: 'Lorem Ipsum, ipsum',
+    id: '1',
+    title: 'TECH CONTRA O TEMPO',
+    description:
+      'As tecnologias estão correndo contra o tempo para avançar entre elas mesmas.',
+    photoCover:
+      'https://www.ucf.edu/wp-content/blogs.dir/20/files/2023/07/Tech-Hub.jpg',
   },
   {
-    id: 2,
-    photo: 'news',
+    id: '2',
+    title: 'News 2024 AI',
+    description:
+      'They revolutionize the workflow by enabling computers to learn.',
+    photoCover:
+      'https://www.nichepursuits.com/wp-content/uploads/2023/05/ai-generators-1200x750.png',
+  },
+  {
+    id: '2',
+    title: 'News AI',
+    description:
+      'IoT refers to the network of interconnected devices that collect and exchange data via the Internet.',
+    photoCover:
+      'https://www.nichepursuits.com/wp-content/uploads/2023/05/ai-generators-1200x750.png',
+  },
+  {
+    id: '3',
     title: 'News AI',
     description: 'Lorem Ipsum, ipsum',
+    photoCover:
+      'https://insight.ng/wp-content/uploads/2022/01/andrey-suslov-shutterstock-1199480788_w400-3.png',
+  },
+  {
+    id: '4',
+    title: 'News AI',
+    description: 'That reason, and make decisions almost like us, humans.',
+    photoCover:
+      'https://apicms.thestar.com.my/uploads/images/2023/12/30/2465136.jpg',
   },
 ];
