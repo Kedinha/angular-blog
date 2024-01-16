5a9cee2256e0a9aca6bb836d33296d6d2ecfb176
kedinha <kedmyknow@gmail.com>
2024-01-16 16:44:43 -0300

    Recovering route parameters (params)

--- Projetos/rotas/src/app/pages/portifolio/card/card.component.ts
+++ Projetos/rotas/src/app/pages/portifolio/card/card.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private activeRoute: ActivatedRoute) {
+    this.activeRoute.params.subscribe((res) => console.log(res));
   }
 
+  ngOnInit(): void {}
 }
