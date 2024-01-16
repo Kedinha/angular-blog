a3cdbf459e1471bde28615b2ef243ab097757b88
kedinha <kedmyknow@gmail.com>
2024-01-16 17:17:23 -0300

    Children routes params

--- Projetos/rotas/src/app/pages/portifolio/card/card.component.ts
+++ Projetos/rotas/src/app/pages/portifolio/card/card.component.ts
@@ -10,6 +10,7 @@ export class CardComponent implements OnInit {
   constructor(private activeRoute: ActivatedRoute, private navegador: Router) {
     // http://localhost:4200/portifolio/{1}
     this.activeRoute.params.subscribe((res) => console.log(res));
+    this.activeRoute.firstChild?.params.subscribe((res) => console.log(res));
 
     // http://localhost:4200/portifolio/1?{name=kedma&token=123}
     this.activeRoute.queryParams.subscribe((res) => console.log(res));
