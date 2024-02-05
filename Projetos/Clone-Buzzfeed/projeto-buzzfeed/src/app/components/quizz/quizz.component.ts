72fe10598a60c87aa51ce6eb6765e6247c1dcdd4
kedinha <kedmyknow@gmail.com>
2024-02-05 14:06:12 -0300

    Quizz Questions

--- Projetos/Clone-Buzzfeed/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ Projetos/Clone-Buzzfeed/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_question from '../../../assets/data/quizz_question.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
+  title: string = '';
 
-  constructor() { }
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    if (quizz_question) {
+      this.finished = false;
+      this.title = quizz_question.title;
 
+      this.questions = quizz_question.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
 }
